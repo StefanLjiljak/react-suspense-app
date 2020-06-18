568c510d0ee10f70c6213f5d7d2fb0249356296f
StefanLjiljak <stefanljiljak@gmail.com>
2020-06-18 16:03:36 +0200

    first commit

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# React Suspense Test App (Data Fetching)
-
-- svakog dana u svakom pogledu sve vise napredujem :)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import { fetchData } from './Api'
+import spinner from '../src/sample.gif'
+
+const resource = fetchData()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <Suspense fallback={<Spinner />}>
+        <ProfileDetails />
+        <ProfilePosts />
+      </Suspense>
     </div>
   );
 }
+
+const ProfileDetails = () => {
+  const user = resource.user.read()
+
+  return (
+    <div className="card card-body my-3">
+      <h1 className="large text-primary">{user.name}</h1>
+      <ul>
+        <li>Username: {user.username}</li>
+        <li>Email: {user.email}</li>
+        <li>City: {user.address.city}</li>
+      </ul>
+    </div>
+  )
+}
+
+
+const ProfilePosts = () => {
+  const posts = resource.posts.read()
+
+  return (
+    <ul className="list-group">
+      <li className="list-group-item"><strong>Latest Posts</strong></li>
+      {posts.map(post => (
+        <li className="list-group-item" key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
+
+const Spinner = () => (
+  <img src={spinner}
+    alt="Loading..."
+    style={{ width: '200px', margin: 'auto', display: "block" }}
+  />
+)
 
 export default App;
